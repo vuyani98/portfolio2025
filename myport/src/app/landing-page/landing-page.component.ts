e78bd54b80c01deca592ec69d73e46b49a7e3f58
unknown <nvuyani307@gmail.com>
2025-02-25 02:28:39 +0200

    mobile response start

--- myport/src/app/landing-page/landing-page.component.ts
+++ myport/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,6 +6,55 @@ import { Component } from '@angular/core';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
+
+  deviceClass = 'desktop';
+  deviceWidth = 0;
+
+  constructor() {
+
+  }
+
+  @HostListener('window:resize', ['$event']) onResize() {
+    this.deviceWidth = window.innerWidth;
+
+    if (this.deviceWidth <= 768) {
+      this.deviceClass = 'mobile';
+    }
+
+    else if (this.deviceWidth <= 1024) {
+      this.deviceClass = 'laptop';
+    }
+
+    else if (this.deviceWidth <= 1440) {
+      this.deviceClass = 'laptop';
+    }
+
+    else {
+      this.deviceClass = 'desktop';
+    }
+  }
+
+
+  ngOnInit(): void {
+
+    this.deviceWidth = window.innerWidth;
+
+    if (this.deviceWidth <= 768) {
+      this.deviceClass = 'mobile';
+    }
+
+    else if (this.deviceWidth <= 1024) {
+      this.deviceClass = 'laptop';
+    }
+
+    else if (this.deviceWidth <= 1440) {
+      this.deviceClass = 'laptop';
+    }
+
+    else {
+      this.deviceClass = 'desktop';
+    }
+  }
 
 }
