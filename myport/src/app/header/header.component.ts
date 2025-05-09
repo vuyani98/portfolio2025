8c09b2ee35de7c386fd6a3eb9116553c903698a3
unknown <nvuyani307@gmail.com>
2025-05-09 14:27:19 +0200

    responsive

--- myport/src/app/header/header.component.ts
+++ myport/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    // Prevent scrolling when menu is open
+    if (this.isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+    document.body.style.overflow = '';
+  }
+
+  // Close menu when clicking outside
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const nav = document.querySelector('nav');
+
+    if (this.isMenuOpen && nav && !nav.contains(target)) {
+      this.closeMenu();
+    }
+  }
+
+  // Close menu when screen size changes to desktop
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    if (window.innerWidth > 768 && this.isMenuOpen) {
+      this.closeMenu();
+    }
+  }
 }
